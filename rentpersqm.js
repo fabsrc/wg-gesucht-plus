456d7e97c40e44569c2bce8b01e5fc49bd3b5c7c
Fabian Schneider <fabbbbbi+git@googlemail.com>
2017-01-22 11:22:34 +0100

    Remove jQuery, Rewrite Code

--- rentpersqm.js
+++ rentpersqm.js
@@ -1,37 +1,60 @@
 // Result Table
-window.$('#table-compact-list tr').each(function () {
-  var rent = window.$('.ang_spalte_miete b', this).text().match(/\d+/g)
-  var size = window.$('.ang_spalte_groesse span', this).text().match(/\d+/g)
-  if (rent && size) {
-    var calc = (rent / size).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })
-    // console.log(calc)
-  }
-  var $clone = window.$('.ang_spalte_groesse', this).clone()
-  if ($clone.prop('tagName') === 'TH') {
-    window.$('a', $clone).text('pro m²').attr('href', '#').css('white-space', 'nowrap').click(function () { return false })
-  } else {
-    window.$('span', $clone).text(calc + '€')
-  }
-  $clone.insertAfter(window.$('.ang_spalte_groesse', this))
-})
+const rows = document.querySelectorAll('#table-compact-list tr')
+
+if (rows) {
+  rows.forEach(row => {
+    const sizeColumn = row.querySelector('.ang_spalte_groesse')
+    const rentEl = row.querySelector('.ang_spalte_miete b')
+    const sizeEl = row.querySelector('.ang_spalte_groesse span')
+    const rent = rentEl && rentEl.innerText.match(/\d+/g)
+    const size = sizeEl && sizeEl.innerText.match(/\d+/g)
+    const calc = rent && size && (rent / size).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 }) || null
+    const clone = sizeColumn && sizeColumn.cloneNode(true)
+
+    if (clone && clone.tagName === 'TH') {
+      const aTag = clone.querySelector('a')
+
+      if (aTag) {
+        aTag.innerText = 'pro m²'
+        aTag.setAttribute('href', '#')
+        aTag.style.setProperty('white-space', 'nowrap')
+        aTag.addEventListener('click', (e) => e.preventDefault())
+      }
+    } else if (clone && clone.tagName === 'TD') {
+      const spanTag = clone.querySelector('span')
+
+      if (spanTag) {
+        spanTag.innerText = `${calc}€`
+      }
+    }
+
+    if (sizeColumn && clone) {
+      sizeColumn.parentNode.insertBefore(clone, sizeColumn.nextElementSibling)
+    }
+  })
+}
 
 // Detail View
-var $table = window.$('#main_column > .row').first()
-if ($table.length) {
-  var $sizeContainer = $table.find('.col-xs-6:nth-child(1)')
-  var size = $sizeContainer.find('.headline').text().match(/\d+/g)
-  $sizeContainer.addClass('col-xs-4').removeClass('col-xs-6')
-  var $rentContainer = $table.find('.col-xs-6:nth-child(2)')
-  var rent = $rentContainer.find('.headline').text().match(/\d+/g)
-  $rentContainer.addClass('col-xs-4').removeClass('col-xs-6')
+const table = document.querySelector('#main_column > .row')
+
+if (table) {
+  const sizeEl = table.querySelector('.col-xs-6:nth-child(1)')
+  const size = sizeEl && sizeEl.querySelector('.headline').innerText.match(/\d+/g)
+  const rentEl = table.querySelector('.col-xs-6:nth-child(2)')
+  const rent = rentEl && rentEl.querySelector('.headline').innerText.match(/\d+/g)
 
   if (rent && size) {
-    var calc = (rent / size).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })
-    // console.log(calc)
-  }
+    sizeEl.classList.remove('col-xs-6')
+    sizeEl.classList.add('col-xs-4')
+    rentEl.classList.remove('col-xs-6')
+    rentEl.classList.add('col-xs-4')
+    const calc = (rent / size).toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })
+    const clone = sizeEl.cloneNode(true)
 
-  var $clone = $sizeContainer.clone()
-  $clone.find('h1').text(calc + '€')
-  $clone.find('h3').text('Miete pro m²')
-  $clone.insertAfter($rentContainer)
+    if (calc && clone) {
+      clone.querySelector('h1').innerText = `${calc}€`
+      clone.querySelector('h3').innerText = 'Miete pro m²'
+      rentEl.parentNode.insertBefore(clone, rentEl.nextElementSibling)
+    }
+  }
 }
